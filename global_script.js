cc84b9a21dfeff8166d89c1d2458567f2498b0f8
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-09-21 16:56:14 -0400

    made the site wide flames come and go dynamically when window is resized

--- global_script.js
+++ global_script.js
@@ -1,15 +1,15 @@
-const fireLoop3 = new Audio("/assets/sounds/fire_loop_1.mp3");
+const fireLoop3 = new Audio('/assets/sounds/fire_loop_1.mp3');
 fireLoop3.loop = true;
 fireLoop3.volume = 0.2;
 
-const ltFl4Wrapper = document.querySelector(".fl4-w-1 .lt-fl4-wr");
-const ltFlame4 = document.querySelector(".fl4-w-1 .lt-fl4-wr .flame4");
-const rtFl4Wrapper = document.querySelector(".fl4-w-1 .rt-fl4-wr");
-const rtFlame4 = document.querySelector(".fl4-w-1 .rt-fl4-wr .flame4");
-const ltFl4WrapperRear = document.querySelector(".fl4-w-2 .lt-fl4-wr");
-const ltFlame4Rear = document.querySelector(".fl4-w-2 .lt-fl4-wr .flame4");
-const rtFl4WrapperRear = document.querySelector(".fl4-w-2 .rt-fl4-wr");
-const rtFlame4Rear = document.querySelector(".fl4-w-2 .rt-fl4-wr .flame4");
+const ltFl4Wrapper = document.querySelector('.fl4-w-1 .lt-fl4-wr');
+const ltFlame4 = document.querySelector('.fl4-w-1 .lt-fl4-wr .flame4');
+const rtFl4Wrapper = document.querySelector('.fl4-w-1 .rt-fl4-wr');
+const rtFlame4 = document.querySelector('.fl4-w-1 .rt-fl4-wr .flame4');
+const ltFl4WrapperRear = document.querySelector('.fl4-w-2 .lt-fl4-wr');
+const ltFlame4Rear = document.querySelector('.fl4-w-2 .lt-fl4-wr .flame4');
+const rtFl4WrapperRear = document.querySelector('.fl4-w-2 .rt-fl4-wr');
+const rtFlame4Rear = document.querySelector('.fl4-w-2 .rt-fl4-wr .flame4');
 
 function getInnerWidth() {
   return window.innerWidth;
@@ -33,25 +33,25 @@ function getRandomNumber(min, max) {
 
 function startFl4sStaggered() {
   ltFlame4.style.setProperty(
-    "--fl4-anim-timing",
-    `${getRandomNumber(1.45, 1.85)}s`
+    '--fl4-anim-timing',
+    `${getRandomNumber(1.45, 1.85)}s`,
   );
-  ltFlame4.classList.add("flame-4-spark");
+  ltFlame4.classList.add('flame-4-spark');
   ltFlame4Rear.style.setProperty(
-    "--fl4-anim-timing",
-    `${getRandomNumber(1.45, 1.85)}s`
+    '--fl4-anim-timing',
+    `${getRandomNumber(1.45, 1.85)}s`,
   );
-  ltFlame4Rear.classList.add("flame-4-spark");
+  ltFlame4Rear.classList.add('flame-4-spark');
   rtFlame4.style.setProperty(
-    "--fl4-anim-timing",
-    `${getRandomNumber(1.45, 1.85)}s`
+    '--fl4-anim-timing',
+    `${getRandomNumber(1.45, 1.85)}s`,
   );
-  rtFlame4.classList.add("flame-4-spark");
+  rtFlame4.classList.add('flame-4-spark');
   rtFlame4Rear.style.setProperty(
-    "--fl4-anim-timing",
-    `${getRandomNumber(1.45, 1.85)}s`
+    '--fl4-anim-timing',
+    `${getRandomNumber(1.45, 1.85)}s`,
   );
-  rtFlame4Rear.classList.add("flame-4-spark");
+  rtFlame4Rear.classList.add('flame-4-spark');
 }
 
 function fadeInFl4s() {
@@ -61,22 +61,41 @@ function fadeInFl4s() {
     }, 30000);
     fireLoop3.play();
   }, 3000);
-  ltFl4Wrapper.classList.add("front-fade-in");
-  rtFl4Wrapper.classList.add("front-fade-in");
-  ltFl4WrapperRear.classList.add("rear-fade-in");
-  rtFl4WrapperRear.classList.add("rear-fade-in");
+  ltFl4Wrapper.classList.add('front-fade-in');
+  rtFl4Wrapper.classList.add('front-fade-in');
+  ltFl4WrapperRear.classList.add('rear-fade-in');
+  rtFl4WrapperRear.classList.add('rear-fade-in');
 }
 
 if (getInnerWidth() > 1200) {
-  populateSlides(ltFlame4, 52, "flame4", "flames", true);
-  populateSlides(rtFlame4, 52, "flame4", "flames", true);
-  populateSlides(ltFlame4Rear, 52, "flame4", "flames", true);
-  populateSlides(rtFlame4Rear, 52, "flame4", "flames", true);
+  populateSlides(ltFlame4, 52, 'flame4', 'flames', true);
+  populateSlides(rtFlame4, 52, 'flame4', 'flames', true);
+  populateSlides(ltFlame4Rear, 52, 'flame4', 'flames', true);
+  populateSlides(rtFlame4Rear, 52, 'flame4', 'flames', true);
   startFl4sStaggered();
   setTimeout(fadeInFl4s, 500);
 }
 
-if (getInnerWidth() < 1200) {
-  document.querySelector(".fl4-w-1").remove();
-  document.querySelector(".fl4-w-2").remove();
-}
+window.addEventListener('resize', () => {
+  if (getInnerWidth() > 1200) {
+    populateSlides(ltFlame4, 52, 'flame4', 'flames', true);
+    populateSlides(rtFlame4, 52, 'flame4', 'flames', true);
+    populateSlides(ltFlame4Rear, 52, 'flame4', 'flames', true);
+    populateSlides(rtFlame4Rear, 52, 'flame4', 'flames', true);
+    startFl4sStaggered();
+    fireLoop3.volume = 0.2;
+    setTimeout(fadeInFl4s, 500);
+  }
+
+  if (getInnerWidth() < 1200) {
+    ltFlame4.innerHTML = '';
+    rtFlame4.innerHTML = '';
+    ltFlame4Rear.innerHTML = '';
+    rtFlame4Rear.innerHTML = '';
+    ltFl4Wrapper.classList.remove('front-fade-in');
+    rtFl4Wrapper.classList.remove('front-fade-in');
+    ltFl4WrapperRear.classList.remove('rear-fade-in');
+    rtFl4WrapperRear.classList.remove('rear-fade-in');
+    fireLoop3.volume = 0;
+  }
+});
